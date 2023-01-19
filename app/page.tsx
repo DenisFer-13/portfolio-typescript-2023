d2a16aa4ca7ebbbb6229ea6553528f5521e460b6
DenisFerreyra <denis.ferreyra@gigigo.com>
2023-01-19 17:50:06 -0300

    [Styles]: add global & home styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-"use client";                     //Esto permite añadir interactividad a la página
+"use client"; //Esto permite añadir interactividad a la página
 import React, { FC } from "react";
+import style from "./page.module.css";
 
 const scrollTo = (top: number) => {
   window.scrollTo({ top: top, behavior: "smooth" });
@@ -8,8 +9,18 @@ const scrollTo = (top: number) => {
 const Home: FC = () => {
   return (
     <>
-      <div>ESTE SERÁ MI DIV PRINCIPAL</div>
-      <button onClick={() => scrollTo(0)}>SUBIR</button>
+      <div className={style.container}>
+        <h2 className={style.h2}>Hi there! I am</h2>
+        <h1 className={style.h1}>DENIS FERREYRA</h1>
+        <h3 className={style.h3}>Full Stack Developer</h3>
+        <p className={style.p}>
+          Passionate about programming, solving logic problems or graphic
+          challenges, when I program, I don't feel like it's a job, I love
+          programming and I feel like I'm playing. I have a lot to offer, if you
+          are interested, contact me by Github or LunkedIn. Regards.
+        </p>
+        <button onClick={() => scrollTo(0)}>SUBIR</button>
+      </div>
     </>
   );
 };
