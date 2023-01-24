e8261ff1e5885ff739c0e3086188ed05e67f23e8
DenisFerreyra <denis.ferreyra@gigigo.com>
2023-01-24 14:40:57 -0300

    [Feature]: add navigation scroll detection

--- app/page.tsx
+++ app/page.tsx
@@ -12,14 +12,15 @@ const Home: FC = () => {
   return (
     <>
       <div className={style.container}>
-        <h2 className={style.h2}>Hi there! I am</h2>
+        <h2 className={style.h2}>Hola a todos! Mi nombre es</h2>
         <h1 className={style.h1}>DENIS FERREYRA</h1>
         <h3 className={style.h3}>Full Stack Developer</h3>
         <p className={style.p}>
-          Passionate about programming, solving logic problems or graphic
-          challenges, when I program, I don't feel like it's a job, I love
-          programming and I feel like I'm playing. I have a lot to offer, if you
-          are interested, contact me by Github or LunkedIn. Regards.
+          Programador apasionado por los desafíos gráficos y de lógica, poseo 2
+          años de experiencia en proyectos reales y académicos o personales,
+          manejo el stack React - Node, sumado a otras tecnologías relacionadas.
+          Tengo mucho para ofrecer y estoy siempre en búsqueda de nuevas
+          oportunidades, no dudes en contactarme. Saludos.
         </p>
       </div>
       <Technologies />
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,11 +1,28 @@
 //@ts-check
 "use client";
 import Link from "next/link";
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { LinksType } from "@/utils/interfaces";
 import * as style from "./navigation.module.css";
 
 const Navigation: FC = () => {
+  const [y, setY] = useState(0);
+  const handleNavigation = (e: any) => {
+    const window = e.currentTarget;
+    if (y > window.scrollY) {
+      console.log("scrolling up");
+    } else if (y < window.scrollY) {
+      console.log("scrolling down");
+    }
+    setY(window.scrollY);
+  };
+
+  useEffect(() => {
+    setY(window.scrollY);
+
+    window.addEventListener("scroll", (e) => handleNavigation(e));
+  }, []);
+
   const internalLinks: LinksType[] = [
     {
       label: "HOME",
@@ -22,7 +39,7 @@ const Navigation: FC = () => {
   ];
 
   return (
-    <header className={style.header}>
+    <header className={y > 0 ? style.headerSticky : style.header}>
       <nav>
         <ul className={style.ul}>
           {internalLinks.map(({ label, route }) => (
