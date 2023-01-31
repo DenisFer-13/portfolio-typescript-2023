623364aea5a6d9c77f38ee9aa399441a7179b832
DenisFerreyra <denis.ferreyra@gigigo.com>
2023-01-30 22:50:50 -0300

    [Style]: add footer style

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,21 +3,10 @@ import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 const About: FC = () => {
   return (
-    <div style={{ marginTop: "200px" }}>
-      <span style={{ display: "flex", justifyContent: "space-evenly" }}>
-        <a href="https://github.com/DenisFer-13" target="_blank">
-          <AiFillGithub
-            size={30}
-            style={{ marginRight: "10px", color: "white" }}
-          />
-        </a>
-        <a href="https://www.linkedin.com/in/denis-ferreyra/" target="_blank">
-          <AiFillLinkedin
-            size={30}
-            style={{ marginLeft: "10px", color: "white" }}
-          />
-        </a>
-      </span>
+    <div>
+      <form>
+        <input ></input>
+      </form>
     </div>
   );
 };
--- components/footer.tsx
+++ components/footer.tsx
@@ -7,26 +7,32 @@ const Footer = () => {
     {
       label: "LINKEDIN",
       route: "https://linkedin.com/in/denis-ferreyra",
+      top: 100,
     },
     {
       label: "GITHUB",
       route: "https://github.com/DenisFer-13",
+      top: 100,
     },
   ];
 
   return (
     <>
       <footer className={style.footer}>
-        <div>Denis Ferreyra | Full Stack Developer</div>
-        <ul className={style.ul}>
-          {externalLInks.map(({ label, route }) => (
-            <li key={route}>
-              <Link href={route} target={"_blank"}>
-                {label}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <div style={{ display: "flex", justifyContent: 'center' }}>
+          <div className={style.divFooter}>
+            Denis Ferreyra | Full Stack Developer
+          </div>
+          <ul className={style.ul}>
+            {externalLInks.map(({ label, route }) => (
+              <li key={route}>
+                <Link href={route} target={"_blank"}>
+                  {label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </footer>
     </>
   );
